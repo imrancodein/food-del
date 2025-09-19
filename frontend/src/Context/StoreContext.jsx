6e610c67e38662ed0bd165f6fee75a3c35081abf
Mohammad Imran | MERN Stack Developer <135727303+imrancodein@users.noreply.github.com>
2025-09-19 11:27:43 -0700

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext=createContext(null);
 
 const StoreContextProvider=(props)=>{
     const[cartItem, setCartItem]=useState({});
-    const url='http://localhost:4000';
+    const url='https://food-del-w9wy.onrender.com';
     const[token,setToken]=useState('')
     
     // const [food_list,setFoodList]=useState([])
